dc30f0fdb8c5ae965eb9e47d6ad24944f162006f
Alexander Ulitin <ulitiy@gmail.com>
2022-10-08 16:41:15 -0300

    improve course and lesson naming

--- src/app/components/PublishTab.tsx
+++ src/app/components/PublishTab.tsx
@@ -9,52 +9,48 @@ import Parse from 'parse'
 const ParseLesson = Parse.Object.extend('Lesson')
 const ParseCourse = Parse.Object.extend('Course')
 
+export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
+
 export function PublishTab() {
 
-  async function makeLesson(lesson, course) {
+  async function makeLesson(lesson, course, serverLesson) {
     const [lessonFile, thumbnailFile] = await Promise.all([
       new Parse.File(`${lesson.coursePath}.${lesson.path}.svg`, Array.from(lesson.file), 'image/svg+xml').save(),
       new Parse.File(`${lesson.coursePath}.${lesson.path}.thumbnail.png`, Array.from(lesson.thumbnail), 'image/png').save(),
     ])
-    const query = new Parse.Query(ParseLesson)
-    query.equalTo('coursePath', lesson.coursePath)
-    query.equalTo('path', lesson.path)
-    let lessonObject: Parse.Object = new ParseLesson() // (await query.first()) || new ParseLesson()
-    if (lessonObject.isNew()) {
-      lessonObject.set('coursePath', lesson.coursePath)
-      lessonObject.set('path', lesson.path)
-      lessonObject.set('name', {en: lesson.path+'.name'})
+    if (serverLesson.isNew()) {
+      serverLesson.set('coursePath', lesson.coursePath)
+      serverLesson.set('path', lesson.path)
+      serverLesson.set('name', {en: capitalize(lesson.path.split('-').join(' '))})
     }
-    lessonObject.set('course', course)
-    lessonObject.set('file', lessonFile)
-    lessonObject.set('thumbnail', thumbnailFile)
-    lessonObject.set('order', lesson.index)
-    return lessonObject
-  }
-
-  async function deleteCourseLessons(coursePath) {
-    const query = new Parse.Query(ParseLesson)
-    query.equalTo('coursePath', coursePath)
-    const lessons = await query.find()
-    return Parse.Object.destroyAll(lessons)
+    serverLesson.set('course', course)
+    serverLesson.set('file', lessonFile)
+    serverLesson.set('thumbnail', thumbnailFile)
+    serverLesson.set('order', lesson.index)
+    return serverLesson
   }
 
   async function publishCourse() {
-    const course = await pluginApi.exportCourse()
-    let [courseObject, thumbnailFile] = await Promise.all([
+    const course = await pluginApi.exportCourse()    
+    let [courseObject, thumbnailFile, serverLessons] = await Promise.all([
       new Parse.Query(ParseCourse).equalTo('path', course.path).first(),
       new Parse.File(`${course.path}.INDEX.thumbnail.png`, Array.from(course.thumbnail), 'image/png').save(),
-      await deleteCourseLessons(course.path), // TODO: delete only lessons that are not in the course
+      new Parse.Query(ParseLesson).equalTo('coursePath', course.path).find(),
     ])
+    // delete lessons that are not in the course anymore
+    const lessonPaths = course.lessons.map(l => l.path)
+    const deletedLessons = serverLessons.filter(l => !lessonPaths.includes(l.get('path')))
+    Parse.Object.destroyAll(deletedLessons) // no need to await
     courseObject = courseObject || new ParseCourse()
     courseObject.set('path', course.path)
-    courseObject.set('name', {en: course.path+'.name'})
-    courseObject.set('description', {en: course.path+'.description'})
     courseObject.set('thumbnail', thumbnailFile)
     if (courseObject.isNew()) {
+      courseObject.set('name', {en: capitalize(course.path.split('-').join(' '))}) // make all languages the same for now, just copy EN
+      courseObject.set('description', {en: ''})
       await courseObject.save()
     }
-    const lessons = await Promise.all(course.lessons.map(lesson => makeLesson(lesson, courseObject)))
+    const lessons = await Promise.all(course.lessons.map(lesson => makeLesson(lesson, courseObject,
+      serverLessons.find(l => l.get('path') === lesson.path) || new ParseLesson())))
     await Parse.Object.saveAll(lessons.concat([courseObject]))
   }
 
