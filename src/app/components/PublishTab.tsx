3979306f6417434cdc282d334b15e004c1cf4541
Alexander Ulitin <ulitiy@gmail.com>
2022-10-08 21:01:02 -0300

    separate publish beta and production buttons

--- src/app/components/PublishTab.tsx
+++ src/app/components/PublishTab.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Button} from 'react-bootstrap'
-import {emit} from '../../events'
-import {pluginApi} from '../../rpc-api'
-import CourseExporter from './CourseExporter'
+import { Button } from 'react-bootstrap'
+import { pluginApi } from '../../rpc-api'
 import LoginForm from './LoginForm'
 import Parse from 'parse'
+import { userStore } from '../models/user'
+import { useSnapshot } from 'valtio'
 
 const ParseLesson = Parse.Object.extend('Lesson')
 const ParseCourse = Parse.Object.extend('Course')
@@ -12,16 +12,28 @@ const ParseCourse = Parse.Object.extend('Course')
 export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
 
 export function PublishTab() {
+  const userSnapshot = useSnapshot(userStore)
 
-  async function makeLesson(lesson, course, serverLesson) {
+  async function makeLesson(lesson, course, serverLesson, debug) {
+    const cp = debug ? `${lesson.coursePath}-debug` : lesson.coursePath
     const [lessonFile, thumbnailFile] = await Promise.all([
-      new Parse.File(`${lesson.coursePath}.${lesson.path}.svg`, Array.from(lesson.file), 'image/svg+xml').save(),
-      new Parse.File(`${lesson.coursePath}.${lesson.path}.thumbnail.png`, Array.from(lesson.thumbnail), 'image/png').save(),
+      new Parse.File(
+        `${cp}.${lesson.path}.svg`,
+        Array.from(lesson.file),
+        'image/svg+xml'
+      ).save(),
+      new Parse.File(
+        `${cp}.${lesson.path}.thumbnail.png`,
+        Array.from(lesson.thumbnail),
+        'image/png'
+      ).save(),
     ])
     if (serverLesson.isNew()) {
-      serverLesson.set('coursePath', lesson.coursePath)
+      serverLesson.set('coursePath', cp)
       serverLesson.set('path', lesson.path)
-      serverLesson.set('name', {en: capitalize(lesson.path.split('-').join(' '))})
+      serverLesson.set('name', {
+        en: capitalize(lesson.path.split('-').join(' ')),
+      })
     }
     serverLesson.set('course', course)
     serverLesson.set('file', lessonFile)
@@ -30,37 +42,65 @@ export function PublishTab() {
     return serverLesson
   }
 
-  async function publishCourse() {
-    const course = await pluginApi.exportCourse()    
+  async function publishCourse(
+    { debug }: { debug: boolean } = { debug: false }
+  ) {
+    const course = await pluginApi.exportCourse()
+    const cp = debug ? `${course.path}-debug` : course.path
     let [courseObject, thumbnailFile, serverLessons] = await Promise.all([
-      new Parse.Query(ParseCourse).equalTo('path', course.path).first(),
-      new Parse.File(`${course.path}.INDEX.thumbnail.png`, Array.from(course.thumbnail), 'image/png').save(),
-      new Parse.Query(ParseLesson).equalTo('coursePath', course.path).find(),
+      new Parse.Query(ParseCourse).equalTo('path', cp).first(),
+      new Parse.File(
+        `${cp}.INDEX.thumbnail.png`,
+        Array.from(course.thumbnail),
+        'image/png'
+      ).save(),
+      new Parse.Query(ParseLesson).equalTo('coursePath', cp).find(),
     ])
     // delete lessons that are not in the course anymore
-    const lessonPaths = course.lessons.map(l => l.path)
-    const deletedLessons = serverLessons.filter(l => !lessonPaths.includes(l.get('path')))
+    const lessonPaths = course.lessons.map((l) => l.path)
+    const deletedLessons = serverLessons.filter(
+      (l) => !lessonPaths.includes(l.get('path'))
+    )
     Parse.Object.destroyAll(deletedLessons) // no need to await
     courseObject = courseObject || new ParseCourse()
-    courseObject.set('path', course.path)
+    courseObject.set('path', cp)
     courseObject.set('thumbnail', thumbnailFile)
     if (courseObject.isNew()) {
-      courseObject.set('name', {en: capitalize(course.path.split('-').join(' '))}) // make all languages the same for now, just copy EN
-      courseObject.set('description', {en: ''})
+      courseObject.set('name', {
+        en: capitalize(cp.split('-').join(' ')),
+      }) // make all languages the same for now, just copy EN
+      courseObject.set('description', { en: '' })
       await courseObject.save()
     }
-    const lessons = await Promise.all(course.lessons.map(lesson => makeLesson(lesson, courseObject,
-      serverLessons.find(l => l.get('path') === lesson.path) || new ParseLesson())))
+    const lessons = await Promise.all(
+      course.lessons.map((lesson) =>
+        makeLesson(
+          lesson,
+          courseObject,
+          serverLessons.find((l) => l.get('path') === lesson.path) ||
+            new ParseLesson(),
+          debug
+        )
+      )
+    )
     await Parse.Object.saveAll(lessons.concat([courseObject]))
   }
 
   return (
     <>
-      <LoginForm/>
-      <Button onClick={publishCourse}>Publish course</Button>
-      <Button className='m-1' onClick={() => emit('exportCourse')}>Export course</Button>
-      <CourseExporter/>
-      <Button onClick={() => emit('generateCode')} className='m-1'>Generate code</Button>
+      <LoginForm />
+      <Button onClick={() => publishCourse({ debug: true })}>
+        Publish debug
+      </Button>{' '}
+      {userSnapshot.user &&
+        userSnapshot.user.get('email') === 'ulitiy@gmail.com' && (
+          <Button
+            variant='danger'
+            onClick={() => publishCourse({ debug: false })}
+          >
+            Publish production
+          </Button>
+        )}
     </>
   )
 }
