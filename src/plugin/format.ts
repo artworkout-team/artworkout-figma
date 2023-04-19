18af5ed40dd64bd72825401087568d6f2364c3ec
Ivan-okolzin <74009153+Ivan-okolzin@users.noreply.github.com>
2023-04-19 15:24:48 +0400

    pr fixes

--- src/plugin/format.ts
+++ src/plugin/format.ts
@@ -80,10 +80,10 @@ function autoFormat() {
   findAll(figma.root, (node) =>
     /^step s-multistep-result/.test(node.name)
   ).forEach((n: GroupNode) => {
-    if (n.children[0].type === 'RECTANGLE') {
-      n.children[0].name = 'template'
-      const nChildren = n.children[0] as unknown as GroupNode
-      nChildren.children[0].name = '/ignore'
+    n.children[0].name = 'template'
+    const templateNode = n.children[0] as GroupNode
+    templateNode.children[0].name = '/ignore'
+    if (n.children[0].type === 'RECTANGLE' && n.children.length == 1) {
       n.resize(40, 40)
       n.x = 10
       n.y = 60
